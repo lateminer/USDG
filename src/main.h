1fad11f94cc4d4430f7ff5416e9229a43da36d0e
USDGcoin <USDG@protonmail.com>
2022-07-20 11:49:41 +0200

    check some more blocks

--- src/main.h
+++ src/main.h
@@ -205,10 +205,10 @@ extern bool fPruneMode;
 /** Number of MiB of block files that we're trying to stay below. */
 extern uint64_t nPruneTarget;
 /** Block files containing a block-height within MIN_BLOCKS_TO_KEEP of chainActive.Tip() will not be pruned. */
-static const unsigned int MIN_BLOCKS_TO_KEEP = 500;
+static const unsigned int MIN_BLOCKS_TO_KEEP = 5000;
 
-static const signed int DEFAULT_CHECKBLOCKS = 50;
-static const unsigned int DEFAULT_CHECKLEVEL = 3;
+static const signed int DEFAULT_CHECKBLOCKS = 241;
+static const unsigned int DEFAULT_CHECKLEVEL = 4;
 
 // Require that user allocate at least 550MB for block & undo files (blk???.dat and rev???.dat)
 // At 1MB per block, 288 blocks = 288MB.
